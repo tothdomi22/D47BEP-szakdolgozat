337795c4f969675ed3a8dea8e3241a29874dcb9c
tothdomi22 <tothdomi22@gmail.com>
2024-06-23 22:15:07 +0200

    feat: index.html api fetch

--- website/index.js
+++ website/index.js
@@ -8,12 +8,29 @@ const Sensor = require('./models/sensor');
 
 
 app.get('/api/data', async (req, res) => {
-    const data = await Sensor.findAll({
+    const data = await Sensor.findOne({
         order: [
             ['createdAt', 'DESC'],
         ],
     });
     res.json(data);
+});
+
+app.get('/api/data/insert', async (req, res) => {
+    try {
+        const data = await Sensor.create(
+            {
+                temperature: 22.3,
+                humidity: 54.3,
+                moisture: 25,
+                waterLevel: 93,
+                light: 239, 
+            },
+        );
+        res.status(200).json({message: "data created successfully"})
+    } catch (error) {
+        res.status(500).json({message: "error uploading data"})
+    }
 });
 
 app.get('/', (req, res) => {

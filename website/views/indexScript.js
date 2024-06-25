21432fd2c7014307dd6895503b4366c667f94ad3
tothdomi22 <tothdomi22@gmail.com>
2024-06-25 21:13:23 +0200

    feat: bootstrap + passport.js install

--- website/views/indexScript.js
+++ website/views/indexScript.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', function(){
     fetch('http://localhost:3000/api/data')
     .then(response => response.json())
     .then(data => {
-        document.getElementById('temperature').innerText = `Temperature: ${data.temperature}`;
-        document.getElementById('humidity').innerText = `Humidity: ${data.humidity}`;
-        document.getElementById('moisture').innerText = `Moisture: ${data.moisture}`;
-        document.getElementById('waterLevel').innerText = `waterLevel: ${data.waterLevel}`;
-        document.getElementById('light').innerText = `light: ${data.light}`;
-        document.getElementById('createdAt').innerText = `Last update: ${data.createdAt}`;
+        document.getElementById('temperature').innerText = `${data.temperature}°C`;
+        document.getElementById('humidity').innerText = `${data.humidity}%`;
+        document.getElementById('moisture').innerText = `${data.moisture}%`;
+        document.getElementById('waterLevel').innerText = `${data.waterLevel}cm`;
+        document.getElementById('light').innerText = `${data.light}lum`;
+        document.getElementById('createdAt').innerText = `${data.createdAt}`;
     })
     .catch(error => console.error('error fetching data:', error));
 });
